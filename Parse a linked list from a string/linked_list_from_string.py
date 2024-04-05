e7aa58a64228374a98c3eb8c9f0ae4164a894c0f
Tetiana Shvets <shvets.pn@ucu.edu.ua>
2024-04-05 18:30:21 +0300

    linked_list_from_string function

---
+++ Parse a linked list from a string/linked_list_from_string.py
@@ -0,0 +1,14 @@
+class Node:
+    def __init__(self, data, next=None): 
+        self.data = data
+        self.next = next
+
+def linked_list_from_string(s):
+    if s=="None":
+        return None
+    lst=s.split(" -> ")
+    lst=lst[::-1]
+    cur=Node(int(lst[1]))
+    for i in lst[2:]:
+        cur=Node(int(i),cur)
+    return cur
